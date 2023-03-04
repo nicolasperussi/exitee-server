44b74ff53f6e83da915698954eed3190e02bcdf9
Nicolas Perussi <nicolasperussi12@hotmail.com>
2023-03-04 01:25:32 -0300

    initialized prisma schema

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log('Hello world!');
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function main() {
+	const user = await prisma.user.create({
+		data: {
+			name: 'Rafael',
+		},
+	});
+	console.log(user);
+}
+
+main();
